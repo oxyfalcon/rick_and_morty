4c71897ff2906ba5d85714693cc5b173f2960188
oxyfalcon <akashsinghcool19990@gmail.com>
2023-08-25 17:30:11 +0530

    temp Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
             colorScheme:
                 ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
